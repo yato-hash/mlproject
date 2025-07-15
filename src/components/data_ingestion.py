aa3e20e8253e04512f2d5b6fab709d904466f871
Isha Singh <singhisha1003@gmail.com>
2025-07-15 09:40:32 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -20,7 +22,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info('Entered the data ingestion method or component')
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -44,4 +46,7 @@ class DataIngestion:
         
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transforamtion = DataTransformation()
+    data_transforamtion.initiate_data_transformation(train_data,test_data)
--- src/exception.py
+++ src/exception.py
@@ -3,8 +3,10 @@ import sys
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = 'Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format()
-    file_name,exc_tb.tb_lineno,str(error)
+    line_number = exc_tb.tb_lineno
+    error_message = 'Error occurred in python script name [{0}] line number [{1}] error message [{2}]'.format(
+    file_name, line_number, str(error)
+    )
 
     return error_message
 
